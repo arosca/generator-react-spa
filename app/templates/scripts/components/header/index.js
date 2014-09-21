18d216ff9a5ec66643670875a7b841f0bcd5a387
arosca <contact@arosca.ro>
2014-09-21 13:57:10 +0300

    ribbon & header styles

--- app/templates/scripts/components/header/index.js
+++ app/templates/scripts/components/header/index.js
@@ -8,12 +8,12 @@ var Header = React.createClass({
     render: function() {
         return (
             <header>
-                <ul className="nav nav-pills pull-right">
+                <h1>React SPA</h1>
+                <ul className="nav nav-pills">
                     <li><Link to="home">Home</Link></li>
                     <li><Link to="about">About</Link></li>
                     <li><Link to="contact">Contact</Link></li>
                 </ul>
-                <h1>React SPA</h1>
             </header>
         );
     }
