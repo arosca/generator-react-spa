e1f31ccb1febfedcd13a1696bbbed339e0cd219b
arosca <contact@arosca.ro>
2014-10-04 11:47:45 +0300

    user message fix

--- app/templates/scripts/actions.js
+++ app/templates/scripts/actions.js
@@ -5,14 +5,14 @@ var Actions = {
 
     increment: function() {
         Dispatcher.handleViewAction({
-            actionType: Const.COUNTER_INCREMENT
+            'actionType': Const.COUNTER_INCREMENT
         });
     },
 
     userMessage: function(message) {
         Dispatcher.handleViewAction({
-            actionType: Const.USER_MESSAGE,
-            text: message
+            'actionType': Const.USER_MESSAGE,
+            'message': message
         });
     }
 
