e1f9294ead2182c99960604a6ce99c5e1268c80c
arosca <contact@arosca.ro>
2014-10-11 15:06:23 +0300

    switched to npm bootstrap; removed image optimization

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -76,11 +76,11 @@ gulp.task('extras', function () {
 
 gulp.task('images', function () {
     return gulp.src('app/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
+        // .pipe($.cache($.imagemin({
+        //     optimizationLevel: 3,
+        //     progressive: true,
+        //     interlaced: true
+        // })))
         .pipe(gulp.dest('dist/images'));
 });
 
